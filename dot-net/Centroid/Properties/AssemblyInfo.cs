c9e1ff59a4598c7c365fa0f6f489b65083181918
Jason Roebuck <jroebuck@resdat.com>
2014-02-06 10:37:01 -0800

    update assembly properties

--- dot-net/Centroid/Properties/AssemblyInfo.cs
+++ dot-net/Centroid/Properties/AssemblyInfo.cs
@@ -6,14 +6,13 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Centroid")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A cross-platform approach to configuration. http://github.com/ResourceDataInc/Centroid")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Centroid")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2014, Resource Data Inc.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
