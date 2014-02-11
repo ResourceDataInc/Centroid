2791dea4c663abde8eada1a93c579cc6753c98a7
Chris Thorn <thorncp@gmail.com>
2014-02-11 12:00:10 -0900

    Simplify the public API

--- dot-net/Centroid.Tests/ConfigTest.cs
+++ dot-net/Centroid.Tests/ConfigTest.cs
@@ -5,8 +5,6 @@ namespace Centroid.Tests
     [TestFixture]
     public class ConfigTest
     {
-        private const string SharedFilePath = @"..\..\..\..\config.json";
-
         private const string JsonConfig = @"
             {
                 ""Dev"": {
@@ -28,13 +26,6 @@ namespace Centroid.Tests
         ";
 
         [Test]
-        public void environment_property_shows_correct_environment()
-        {
-            var config = new Config(JsonConfig).WithEnvironment("Prod");
-            Assert.AreEqual("Prod", config.Environment);
-        }
-
-        [Test]
         public void environment_specific_config_is_included_correctly()
         {
             var config = new Config(JsonConfig).WithEnvironment("Dev");
@@ -53,16 +44,6 @@ namespace Centroid.Tests
         {
             dynamic config = new Config(@"{ ""snake_key"": ""some value"" }");
             Assert.AreEqual("some value", config.SnakeKey);
-        }
-
-        [Test]
-        public void can_load_config_from_file()
-        {
-            Assert.DoesNotThrow(() =>
-                {
-                    dynamic config = Config.FromFile(SharedFilePath);
-                    Assert.NotNull(config.All);
-                });
         }
     }
 }
