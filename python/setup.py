825b93e66bad97a52ec21b82b8c3b6d6a29c5e53
Greg Scott <i@gregoryjscott.com>
2014-02-14 15:20:36 -0900

    make the metadata for packages match

--- python/setup.py
+++ python/setup.py
@@ -2,14 +2,10 @@
 
 from distutils.core import setup
 
-# markdown isn't supported, so this'll be janky
-with open('README.md') as file:
-  long_description = file.read()
-
 setup(name = 'centroid',
       version = '1.0.0',
-      description = 'A centralized paradigm on configuration management.',
-      long_description = long_description,
+      description = 'A centralized paradigm to configuration management.',
+      long_description = 'Centroid is library for loading configuration values declared in JSON, and accessing those configuration values using object properties.',
       author = 'Resource Data, Inc',
       author_email = 'oss@resdat.com',
       url = 'https://github.com/ResourceDataInc/Centroid',
